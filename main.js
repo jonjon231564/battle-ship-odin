7abb5c477c316512d22a813c84677d54d1af85a1
Jonathan Pitts <jon.pitts9@gmail.com>
2024-08-01 10:52:09 -0500

    First commit. Gameboard now places ships within itself upon creation

---
+++ main.js
@@ -0,0 +1,93 @@
+function isArray(value) {
+    return Array.isArray(value);
+}
+
+class Ship {
+    constructor(length, type, hits = 0, sunk = false) {
+        this.length = length;
+        this.type = type;
+        this.hits = hits;
+        this.sunk = sunk;
+    }
+    hit() {
+        this.hits++;
+    }
+    isSunk() {
+        if (this.hits === this.length) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
+
+class Gameboard {
+    constructor() {
+        this.gameboard = [
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        ];
+        this.ships = [
+            new Ship(5, "ca"),
+            new Ship(4, "ba"),
+            new Ship(3, "cr"),
+            new Ship(3, "su"),
+            new Ship(2, "de"),
+        ];
+        this.placeShips()
+    }
+    placeShips(shipObject) {
+        // for loop for each ship
+        for(let i = 0; i < this.ships.length; i++) {
+            let array = []
+            let arrayQueue = []
+            // while loop that is completed once a ship placement is valid
+            while(true) {
+                let a = Math.floor(Math.random() * 10)
+                let b = Math.floor(Math.random() * 10)
+                let randomDirection = Math.floor(Math.random() * 2)
+
+                arrayQueue = []
+                // for loop that is for each coordinate array of the ship placement
+                for(let j = 0; j < this.ships[i].length; j++) {
+                    if(a > 9 || b > 9) {
+                        break
+                    }
+                    // if else that makes sure the coordinate is valid
+                    if(this.gameboard[a][b] === 0) {
+                        arrayQueue[j] = [a, b]
+                        if (randomDirection == 0) {
+                            a++
+                        } else {
+                            b++
+                        }
+                    } else {
+                        break
+                    }
+                }
+                if(arrayQueue.length == this.ships[i].length) {
+                    break
+                }
+            }
+            array = [...arrayQueue]
+            for(let x = 0; x < array.length; x++) {
+                this.gameboard[array[x][0]][array[x][1]] = this.ships[i].type
+            }
+        }
+        console.log(this.gameboard)
+    }
+    
+}
+
+const gameboard = new Gameboard();
+
+
+module.exports = { Ship, Gameboard };
